683568c9ceec3d05b374cd27eb215aa27ad8f0e5
Sammam Mahdi <sammam.mahdi@gmail.com>
2025-07-25 18:09:51 +0600

    bg-edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 import { AnimatedRobot } from "./components/AnimatedRobot";
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   );
 }
